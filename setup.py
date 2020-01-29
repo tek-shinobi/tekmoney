0cd07f6879492ffbd68b2604b13b430fab67a168
surya-durgaputra <surya.durgaputra@gmail.com>
2020-01-29 16:17:28 +0400

    changed setuu.py status

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     ],
     platforms=['any'],
     classifiers=[
-        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
